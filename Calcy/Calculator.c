aea37198feca6fa5c51493768f10cd0c23a90e91
Shradha <shradha16.edu@gmail.com>
2022-10-01 14:20:15 +0530

    added raise to operator to find exponent of numbers

--- Calcy/Calculator.c
+++ Calcy/Calculator.c
@@ -1,49 +1,60 @@
-#include<stdio.h>  
+#include<stdio.h>
 #include<conio.h>
-void main(){  
-    int num1,num2;  
-    char sign,restart;  
-    do{     
-        printf("Number 1 : ");  
-        scanf("%d",&num1);  
-        printf("\nOperator : "); 
-        scanf(" %c",&sign);  
-        zero:  
-            printf("\nNumber 2 : ");  
-            scanf("%d",&num2);  
-        switch(sign){  
-            case'+':  
-                printf("\nSum = %d ",num1+num2);  
-                break;  
-            case'-':  
-                printf("\nDifference = %d ",num1-num2);  
-                break;  
-            case'*':  
-                printf("\nProduct = %d ",num1*num2);  
-                break;  
-            case '%':  
-                printf("\nRemainder = %d",num1%num2);   
-                break;  
-            case'/':  
-                while(1){  
-                    if(num2==0){  
-                        printf("\nNumber 2 Can't Be Zero, Try Again!!!");   
-                        goto zero;}  
+
+void main(){
+    int num1,num2;
+int result;
+    char sign,restart;
+    do{
+        printf("Number 1 : ");
+        scanf("%d",&num1);
+        printf("\nOperator : ");
+        scanf(" %c",&sign);
+        zero:
+            printf("\nNumber 2 : ");
+            scanf("%d",&num2);
+
+        switch(sign){
+            case'+':
+                printf("\nSum = %d ",num1+num2);
+                break;
+            case'-':
+                printf("\nDifference = %d ",num1-num2);
+                break;
+            case'*':
+                printf("\nProduct = %d ",num1*num2);
+                break;
+            case'^':
+                result = 1;
+                while (num2 != 0) {
+                    result *= num1;
+                    --num2;
+                }
+                printf("Exponent = %d", result);
+                break;
+            case '%':
+                printf("\nRemainder = %d",num1%num2);
+                break;
+            case'/':
+                while(1){
+                    if(num2==0){
+                        printf("\nNumber 2 Can't Be Zero, Try Again!!!");
+                        goto zero;}
                     else{
                         break;}
-                    }  
+                    }
                 printf("\nQuotient = %.2f ",(float)num1/num2);
-                break;  
-            default :  
-                printf("\nInvalid Operator :/");   
-                break;}  
-        printf("\n-------------------");  
+                break;
+            default :
+                printf("\nInvalid Operator :/");
+                break;}
+        printf("\n-------------------");
         printf("\nRestart? : ");
-        scanf(" %c",&restart);  
-        printf("-------------------\n");  
-    }while(restart=='y');  
+        scanf(" %c",&restart);
+        printf("-------------------\n");
+    }while(restart=='y');
 getch();
-} 
+}
 
 
 
